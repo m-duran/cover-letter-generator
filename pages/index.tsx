bc195853074794d0d63cfe9d4a1c9c786361863c
Miguel Duran <mduran0314@hotmail.com>
2023-02-12 19:30:26 -0800

    Generate cover letter based on submitted role

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
 
-  async function onSubmit(event) {
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     try {
       const response = await fetch("/api/generate", {
@@ -14,17 +14,20 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ role: animalInput }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
       setResult(data.result);
       setAnimalInput("");
-    } catch(error) {
+    } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
